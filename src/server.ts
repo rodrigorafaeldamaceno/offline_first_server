6abe66674c97eb9e44cc737691b396e06a505148
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2021-01-26 18:27:55 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import express, { Request, Response } from "express";
 import routes from "./routes/routes";
 import "./database/database";
 
+var port = process.env.PORT || 3333;
 
 const app = express();
-
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(port);
